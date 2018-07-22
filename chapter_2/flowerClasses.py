18a7b47e82e63b815d8ab355cf7bb03cd21afabd
Chiraag Gohel <cgohel@gmail.com>
2018-07-22 02:19:41 -0400

    Added flowerClasses

---
+++ chapter_2/flowerClasses.py
@@ -0,0 +1,6 @@
+import pandas as pd
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header = None)
+df.tail()
+
+import matplotlib.pyplot as plt
+import numpy as np
